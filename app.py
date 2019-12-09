410c5bd25d6e206c537a3239004def6270b58a85
Anton Zavyalov <zavanton123@gmail.com>
2019-12-09 20:48:30 +0300

    login start

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+
+class LoginForm(Form):
+    username = StringField("Username: ", validators=[DataRequired(), Length(1, 16)])
+    password = PasswordField("Password: ", validators=[DataRequired(), Length(1, 20)])
+    login = SubmitField("Login")
 
 
 @app.route("/")
 def home():
     user = "zavanton"
     return render_template("home.html", user=user)
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
 @app.route("/profile")
