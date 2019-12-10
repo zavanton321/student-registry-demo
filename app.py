3c5f44cefcdfe7fb54f9984414d2dc581440f272
Anton Zavyalov <zavanton123@gmail.com>
2019-12-10 21:25:03 +0300

    auth

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, redirect, url_for, request
 from flask_bootstrap import Bootstrap
-from flask_login import UserMixin
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import Form
 from werkzeug.security import generate_password_hash, check_password_hash
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
@@ -12,6 +12,8 @@ app.config['SECRET_KEY'] = "SOME_KEY"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.sqlite3"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bootstrap = Bootstrap(app)
+loginManager = LoginManager(app)
+loginManager.login_view = "login"
 db = SQLAlchemy(app)
 
 
@@ -32,14 +34,14 @@ class StudentForm(Form):
 
 class UserEntity(UserMixin, db.Model):
     __tablename__ = "users"
-    user_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
     password_hash = db.Column(db.String)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-    def get_password_hash(self, password):
+    def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
     @staticmethod
@@ -48,12 +50,19 @@ class UserEntity(UserMixin, db.Model):
         user.set_password(password)
         db.session.add(user)
         db.session.commit()
+        return user
 
 
 class LoginForm(Form):
     username = StringField("Username: ", validators=[DataRequired(), Length(1, 16)])
     password = PasswordField("Password: ", validators=[DataRequired(), Length(1, 20)])
+    remember_me = BooleanField("Remember me")
     login = SubmitField("Login")
+
+
+@loginManager.user_loader
+def load_user(id):
+    return UserEntity.query.get(int(id))
 
 
 @app.before_first_request
@@ -76,6 +85,7 @@ def home():
 
 
 @app.route("/student", methods=["GET", "POST"])
+@login_required
 def student():
     username = None
     first_name = None
@@ -114,14 +124,26 @@ def save_student(first_name, last_name, username):
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    username = None
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        username = login_form.username.data
-    return render_template("login.html", login_form=login_form, username=username)
+        user = UserEntity.query.filter_by(username=login_form.username.data).first()
+        if user is not None and user.check_password(login_form.password.data):
+            login_user(user, login_form.remember_me.data)
+            return redirect(request.args.get("next") or url_for("home"))
+        else:
+            return redirect(url_for("login", **request.args))
+    return render_template("login.html", login_form=login_form)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
 
 
 @app.route("/profile")
+@login_required
 def profile():
     return render_template("profile.html")
 
